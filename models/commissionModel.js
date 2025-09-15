e21fd83ba07642832ef9ad77ff7c0447545a633c
niranjan <niranjan@7unique.in>
2025-09-15 15:45:51 +0530

    ad

--- models/commissionModel.js
+++ models/commissionModel.js
@@ -16,6 +16,22 @@ const commissionSlabSchema = new mongoose.Schema(
         "Sun Direct",
       ],
     },
+    // provider: {
+    //   type: String,
+    //   enum: [
+    //     "billAvenue",
+    //     "spritVerify",
+    //     "serverMaintenance",
+    //     "Mobikwik",
+    //     "A2",
+    //     "A3",
+    //     "A4",
+    //     "A5",
+    //     "A6",
+    //     "A7",
+    //     "A8",
+    //   ],
+    // },
     minAmount: { type: Number, required: true },
     maxAmount: { type: Number, required: true },
     commissionType: {
@@ -39,13 +55,17 @@ const commissionPackageSchema = new mongoose.Schema(
   {
     service: {
       type: String,
-      enum: ["DMT", "AEPS", "PAYIN", "PAYOUT","Mobile Recharge","DTH"],
+      enum: ["DMT", "AEPS", "PAYIN", "PAYOUT", "Mobile Recharge", "DTH"],
       required: true,
     },
     packageName: { type: String, required: true, unique: true },
     gst: { type: Number, default: 18 },
     tds: { type: Number, default: 5 },
     slabs: [commissionSlabSchema],
+    isDefault: {
+      type: Boolean,
+      default: false,
+    },
     isActive: { type: Boolean, default: true },
     dmtPennyDrop: { type: Number, default: 0 },
     aepsOnboarding: { type: Number, default: 0 },
