4fbd599660153a22f29eaa949f38f58a26b14cbc
niranjan <niranjan@7unique.in>
2025-09-28 12:13:43 +0530

    assets and loan

--- services/cornjob.js
+++ services/cornjob.js
@@ -4,7 +4,7 @@ import userModel from "../models/userModel.js";
 // Every day at midnight
 export const planCheckCronJob = () => {
   // cron.schedule("*/30 * * * * *", async () => {
-    cron.schedule("0 0 * * *", async () => {
+  cron.schedule("0 0 * * *", async () => {
     console.log("🔄 Running daily plan expiry check...");
 
     try {
