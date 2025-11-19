36ad6e1b4fecc92949dfeb186665c25314d3130d
niraj8002 <niranjan7unique.in>
2025-11-19 16:26:39 +0530

    update configuration and enhance login email functionality; add device tracking and error handling improvements

--- config/pan.config.js
+++ config/pan.config.js
@@ -1,9 +1,13 @@
 module.exports = {
-  entityCode: "ABCD000001",
-  secretKey: "12345678",
-  entitySource: "ent01",
-  agentCode: "ag01",
-  campaignId: "",
-  redirectUrl: "",
-  PAN_API_URL: "https://test1.tin-nsdl.com/paam/redirectToFullFormApp.html",
+  entityCode:"e02",
+  secretKey:"abcdefgh",
+  entitySource:"entSrc01",
+  agentCode:"agtCd01",
+  campaignId:"camp12345",
+  redirectUrl:"https://test1.tin-nsdl.com/paam/redirectToFullFormApp.html",
+  PAN_API_URL:"https://test1.tin-nsdl.com/paam/redirectToFullFormApp.html",
 };
+
+// certPwd=123456
+// certAlias=1
+// certName=signing_private_key.pfx
