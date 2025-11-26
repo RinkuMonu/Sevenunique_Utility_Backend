91be821b2ae2f39cf9bb490d03ca098ed382e0ef
vijaysingh7878 <vijaysinghshekhawat9928@gmail.com>
2025-11-26 15:44:19 +0530

    edit

--- models/userModel.js
+++ models/userModel.js
@@ -24,6 +24,12 @@ const userSchema = new mongoose.Schema(
     outletId: {
       type: String
     },
+    aepsInstantPayLat: {
+      type: String
+    },
+    aepsInstantPayLng: {
+      type: String
+    },
     aepsInstantPayBio: {
       type: String,
       enum: ["Pending", "Progress", "Success"],
